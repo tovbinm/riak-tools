2db16da6d6081866924c6676c49b4a157a20198c
Matthew Tovbin <tovbinm@gmail.com>
2012-08-10 11:31:30 -0700

    Add retries support for dump tool

--- src/main/scala/com/riak/tools/Dump.scala
+++ src/main/scala/com/riak/tools/Dump.scala
@@ -20,7 +20,8 @@ object Dump {
 			output: String = "dump.out",
 			keyPrefixes: String = "",
 			keysAlphabet: String = "0123456789abcdefghjkmnpqrstvwxyz", //Crockford Base32
-			keysAlphabetEnding: String = "~",			
+			keysAlphabetEnding: String = "~",
+			retries: Int = 3,
 			sep: String = "\n")
 			
 	def main(args: Array[String]) {
@@ -35,6 +36,8 @@ object Dump {
     			{(v: String, c: Config) => c.copy(keysAlphabet = v)},
     		opt("ke", "keysAlphabetEnding", "s", "source keys alphabet ending. Must be > last letter in alphabet. Default: %s".format(Config().keysAlphabetEnding)) 
     			{(v: String, c: Config) => c.copy(keysAlphabetEnding = v)},
+    		intOpt("r", "numOfRetries", "number of retries is case of failure. Default: %s".format(Config().retries)) 
+    			{(v: Int, c: Config) => c.copy(retries = v)},
     		opt("s", "separator", "s", "output file separator. Default: \\n")
     			{(v: String, c: Config) => c.copy(sep = v)},    		
     		arg("bucket", "a bucket name to copy") {(v: String, c: Config) => c.copy(bucket = v)}
@@ -55,16 +58,26 @@ object Dump {
 			var (curr, totalKeys) = (1, 0)
 			keyRanges foreach { range => {
 				print("Processing key range [%s, %s] (%d out of %d) ...".format(range._1, range._2, curr, totalRanges))
-				var count = 0
-				sc.keysRange(config.bucket, range._1, range._2) foreach { k => out.write(k + config.sep); count += 1 }
+				val count = dumpRange(sc, out, config, range._1, range._2)
 				println(" Dumped %d keys.".format(count))
 				totalKeys += count; curr += 1
 			}}
 			println("Done. Dumped %d keys in %d ranges".format(totalKeys, totalRanges))
 			out.close()
 		}
-		finally { sc.shutdown() }
-		
+		finally { sc.shutdown() }		
+	}
+	def dumpRange(sc: RiakClient, out: PrintWriter, config: Config, from: String, to: String): Int = {
+	  var count = 0
+	  var success = false
+	  (0 until config.retries).toStream.takeWhile(_ => !success).foreach { _ =>
+	    try {
+			sc.keysRange(config.bucket, from, to) foreach { k => out.write(k + config.sep); count += 1 }
+			success = true
+		}
+		catch { case e => { println("dumpRange: Error occured - " + e.getMessage); e.printStackTrace } }
+	  }
+	  count
 	}
 
 }
